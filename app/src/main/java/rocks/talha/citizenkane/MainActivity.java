e4b958dfef1ae8158d644b09b70b7e5fa38b1214
mdtalhachy <talhachowdhury3.1416@gmail.com>
2021-06-15 06:11:06 +0600

    Primary Finish

---
+++ app/src/main/java/rocks/talha/citizenkane/MainActivity.java
@@ -0,0 +1,75 @@
+package rocks.talha.citizenkane;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import rocks.talha.citizenkane.databinding.ActivityMainBinding;
+import rocks.talha.citizenkane.model.Question;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    private int currentQuestionIndex = 0;
+
+
+    private Question[] questionBank = new Question[] {
+
+            //create new question objects
+            new Question(R.string.question_amendments, false),
+            new Question(R.string.question_constitution, true),
+            new Question(R.string.question_declaration, true),
+            new Question(R.string.question_independence_rights, true),
+            new Question(R.string.question_religion, true),
+            new Question(R.string.question_government, false),
+            new Question(R.string.question_government_feds, false),
+            new Question(R.string.question_government_senators, true),
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.questionTextView.setText(questionBank[currentQuestionIndex].getAnswerResID());
+
+        binding.trueButton.setOnClickListener(view -> checkAnswer(true));
+        binding.falseButton.setOnClickListener(view -> checkAnswer(false));
+
+        binding.nextButton.setOnClickListener(view -> {
+            currentQuestionIndex = (currentQuestionIndex + 1) % questionBank.length;
+            updateQuestion();
+        });
+
+        binding.prevButton.setOnClickListener(view -> {
+            if(currentQuestionIndex > 0){
+                currentQuestionIndex = (currentQuestionIndex - 1) % questionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void checkAnswer(boolean userChoseCorrect){
+        boolean answerIsCorrect = questionBank[currentQuestionIndex].isAnswerTrue();
+        int messageId;
+
+        if(answerIsCorrect == userChoseCorrect){
+            messageId = R.string.correct_answer;
+        }else {
+            messageId = R.string.wrong_answer;
+        }
+
+        Snackbar.make(binding.imageView, messageId, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void updateQuestion() {
+        binding.questionTextView.setText(questionBank[currentQuestionIndex].getAnswerResID());
+    }
+}
